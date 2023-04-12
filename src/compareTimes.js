92bd7191f001a93e7e45bee0d1f8bb4e8f59158d
CursedMushroom <osl2877@rit.edu>
2023-04-12 16:40:22 -0400

    created compare times and getclassinfo functions with temperary schedule class

---
+++ src/compareTimes.js
@@ -0,0 +1,52 @@
+class Schedule {
+    constructor(name, startTime, endTime) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+};
+
+
+const schedules = [
+    new Schedule('classD', 12, 15),
+    new Schedule('classA', 14, 16),
+    new Schedule('classB', 17, 18),
+    new Schedule('classC', 19, 20),
+]
+
+
+///---------------Old Functions-----------------///
+// const compareTimes = (startA, startB, endA, endB) => {
+//     if (!(startA >= endB) && !(endA <= startB)) {
+//         console.log('Times conflict')
+//     } else {
+//         console.log('No Time Conflict');
+//     }
+// }
+// const getClassInfo = (readClass) => {
+//     console.log(
+//         `Class Name: ${readClass.name}, Class Time: ${readClass.startTime} - ${readClass.endTime}`
+//     )
+
+// }
+
+// const compareTimes = (startA, startB, endA, endB) => {
+//     const isConflict = !(startA >= endB) && !(endA <= startB);
+//     return isConflict ? 'Times conflict' : 'No time conflict';
+//   };
+///---------------------------------------------------------///
+
+const compareTimes = (schedA, schedB) => {
+    const isConflict = !(schedA.startTime >= schedB.endTime) && !(schedA.endTime <= schedB.startTime);
+    return isConflict ? 'Times conflict' : 'No time conflict';
+};
+
+const getClassInfo = (readClass) => {
+    return `Class Name: ${readClass.name}, Class Time: ${readClass.startTime} - ${readClass.endTime}`;
+};
+
+
+console.log(compareTimes(schedules[0], schedules[1]));//conflict
+console.log(compareTimes(schedules[0], schedules[2]));//no conflict
+
+console.log(getClassInfo(schedules[0]));//classD, 12-15
