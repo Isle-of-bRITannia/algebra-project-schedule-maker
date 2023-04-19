54f5579098f1d2f82f20eae4c898c2e7ec9f569e
Jordan-Reeves <jar5927@RIT.edu>
2023-04-19 18:06:50 -0400

    Got Edit working!

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { display } from './visualize/display.js';
 
 //pop up varibles
 const popAdd = document.querySelector('#pop-media-add');
+// default set to disabled
 popAdd.setAttribute('disabled', 'disabled');
 const popEdit = document.querySelector('#pop-media-edit');
 const popAddClose = document.querySelector('#pop-media-cancel');
@@ -12,16 +13,23 @@ const popClear = document.querySelector('#pop-media-clear');
 const popEditClear = document.querySelector('#pop-media-editclear');
 const popup = document.querySelector('#show-popup');
 
+// Reference to the table
 const scheduleTable = document.querySelector('#scheduleTable');
 
-let currentRow = 5;
-let daysOfTheWeek = ["async", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-document.querySelectorAll(".classInfo").forEach(
-    (i) => i.addEventListener('click', () => {
-        editpopUp();
-    })
-)
+// edit fields refrences var names
+const editFieldName = document.querySelector('#edit-class-name');
+const editFieldStartTime = document.querySelector('#edit-start-time');
+const editFieldEndTime = document.querySelector('#edit-end-time');
+const editFieldLocation = document.querySelector('#edit-class-room');
+const editFieldDay = document.querySelector('#edit-class-day');
+const editFieldProfessor = document.querySelector('#edit-class-professor');
+const editFieldCredits = document.querySelector('#edit-class-credits');
 
+// Current schedule
+let res;
+let currSchedId;
+
+// default/placeholder Schedule
 let scheduleA =
     Schedule.create([
         {
@@ -63,8 +71,28 @@ let scheduleA =
                 credits: 5,
                 location: "MSS-1275",
                 professor: "Travis",
+            },
+            {
+                name: "classC",
+                startTime: '05:40',
+                endTime: '22:20',
+                day: "Wednesday",
+                credits: 5,
+                location: "MSS-1275",
+                professor: "Travis",
+            },
+            {
+                name: "classD",
+                startTime: '05:40',
+                endTime: '22:20',
+                day: "Wednesday",
+                credits: 5,
+                location: "MSS-1275",
+                professor: "Travis",
             }
+
         ]);
+
 
 const clearAddFields = () => {
     document.querySelector('#class-name').value = "";
@@ -74,10 +102,9 @@ const clearAddFields = () => {
     document.querySelector('#class-location').value = '';
     document.querySelector('#class-professor').value = '';
     document.querySelector('#class-credits').value = '';
-
 }
 
-//toggle popup on click
+//toggle popups on click
 const addpopUp = () => {
     document.querySelector('#popup-1').classList.toggle('active');
     clearAddFields();
@@ -86,42 +113,46 @@ const editpopUp = () => {
     document.querySelector('#popup-2').classList.toggle('active');
 }
 
-const createNewClass = () => {
-    scheduleA =
-        Schedule.create(
-            display()(scheduleA)
-            ,
-            [
-                {
-                    name: document.querySelector('#class-name').value,
-                    startTime: document.querySelector('#start-time').value,
-                    endTime: document.querySelector('#end-time').value,
-                    day: document.querySelector('#class-day').value,
-                    location: document.querySelector('#class-location').value,
-                    professor: document.querySelector('#class-professor').value
-                }
-            ]);
+// Sort the classes by time
+const sortByTime = (classA, classB) => {
+    const classAStartTimeSplit = classA.startTime.split(':');
+    const classAStartTimeH = classAStartTimeSplit[0];
+    const classAStartTimeM = classAStartTimeSplit[1];
+
+    const classAStartTime = Number(classAStartTimeH + classAStartTimeM);
+
+    const classBStartTimeSplit = classB.startTime.split(':');
+    const classBStartTimeH = classBStartTimeSplit[0];
+    const classBStartTimeM = classBStartTimeSplit[1];
+
+    const classBStartTime = Number(classBStartTimeH + classBStartTimeM);
+    return classAStartTime - classBStartTime;
+}
+
+// Create the table based on the schedule
+const makeTable = (res) => {
     // remove all current rows
     document.querySelectorAll(".rowOfClasses").forEach(
         (r) => { r.remove() }
     );
-    const res = display()(scheduleA);
+
     if (Schedule.validate(res.oldSchedule)) {
         console.log(res);
+
         // make array of classers per day
-        const monClasses = res.filter((c) => c.day === "Monday");
-        const tuesClasses = res.filter((c) => c.day === "Tuesday");
-        const wedClasses = res.filter((c) => c.day === "Wednesday");
-        const thursClasses = res.filter((c) => c.day === "Thursday");
-        const friClasses = res.filter((c) => c.day === "Friday");
-        const satClasses = res.filter((c) => c.day === "Saturday");
+        const monClasses = res.filter((c) => c.day === "Monday").sort(sortByTime);
+        const tuesClasses = res.filter((c) => c.day === "Tuesday").sort(sortByTime);
+        const wedClasses = res.filter((c) => c.day === "Wednesday").sort(sortByTime);
+        const thursClasses = res.filter((c) => c.day === "Thursday").sort(sortByTime);
+        const friClasses = res.filter((c) => c.day === "Friday").sort(sortByTime);
+        const satClasses = res.filter((c) => c.day === "Saturday").sort(sortByTime);
 
         // finding the day with the most classes
         const numClasses = [monClasses.length, tuesClasses.length, wedClasses.length, thursClasses.length, friClasses.length, satClasses.length];
         const mostClasses = Math.max(...numClasses);
 
 
-        // make the row elements function
+        // make the row elements function - how to display
         const makeCell = (classItem, rowElement) => {
             if (classItem) {
                 let newCell = rowElement.insertCell();
@@ -157,8 +188,10 @@ const createNewClass = () => {
             }
         }
 
+        // make rows
         for (let i = 0; i < mostClasses; i++) {
             const newRow = scheduleTable.insertRow();
+            newRow.classList.add("rowOfClasses");
 
             makeCell(monClasses[i], newRow);
             makeCell(tuesClasses[i], newRow);
@@ -169,39 +202,137 @@ const createNewClass = () => {
 
         }
     }
-    addpopUp();
+
+    // add on click for each class
+    document.querySelectorAll(".classInfo").forEach(
+        (i) => i.addEventListener('click', (e) => {
+            editpopUp();
+            const currClass = res.find((c) => c.id == e.target.id);
+            currSchedId = e.target.id
+            // load in current data
+            editFieldName.value = currClass.name;
+            editFieldStartTime.value = currClass.startTime;
+            editFieldEndTime.value = currClass.endTime;
+            editFieldDay.value = currClass.day;
+            editFieldLocation.value = currClass.location;
+            editFieldProfessor.value = currClass.professor;
+            editFieldCredits.value = currClass.credits;
+
+        })
+    );
 }
 
+
+// onload display data
+res = display()(scheduleA);
+makeTable(res);
+
+
+// Create a new class
+const createNewClass = () => {
+    scheduleA =
+        Schedule.create(
+            display()(scheduleA)
+            ,
+            [
+                {
+                    name: document.querySelector('#class-name').value,
+                    startTime: document.querySelector('#start-time').value,
+                    endTime: document.querySelector('#end-time').value,
+                    day: document.querySelector('#class-day').value,
+                    location: document.querySelector('#class-location').value,
+                    credits: document.querySelector('#class-credits').value,
+                    professor: document.querySelector('#class-professor').value
+                }
+            ]);
+    res = display()(scheduleA);
+    makeTable(res);
+
+}
+
+const editClass = () => {
+    // Edit each value based on the fields current value
+    const scheduleEdited = Schedule.editName(
+        Schedule.editStartTime(
+            Schedule.editEndTime(
+                Schedule.editDay(
+                    Schedule.editLocation(
+                        Schedule.editProfessor(
+                            Schedule.editCredits(scheduleA,
+                                currSchedId, editFieldCredits.value),
+                            currSchedId, editFieldProfessor.value),
+                        currSchedId, editFieldLocation.value),
+                    currSchedId, editFieldDay.value),
+                currSchedId, editFieldStartTime.value)
+            , currSchedId, editFieldEndTime.value),
+        currSchedId, editFieldName.value
+    );
+    res = display()(scheduleEdited);
+
+    editpopUp();
+    makeTable(res);
+}
+
+
 //disable the addClass button if the fields are empty
-const addClassButton = () => {
+const disableAddClassBttn = () => {
     if (document.querySelector('#class-name').value.trim() !== "" &&
         document.querySelector('#start-time').value.trim() !== "" &&
         document.querySelector('#end-time').value.trim() !== "" &&
         document.querySelector('#class-day').value.trim() !== "" &&
         document.querySelector('#class-location').value.trim() !== "" &&
+        document.querySelector('#class-credits').value.trim() !== "" &&
         document.querySelector('#class-professor').value.trim() !== "") {
         popAdd.removeAttribute('disabled');
     } else {
         popAdd.setAttribute('disabled', 'disabled');
     }
 }
+//disable the editClass button if the fields are empty
+const disableEditClassBttn = () => {
+    if (editFieldName.value.trim() !== "" &&
+        editFieldStartTime.value.trim() !== "" &&
+        editFieldEndTime.value.trim() !== "" &&
+        editFieldDay.value.trim() !== "" &&
+        editFieldLocation.value.trim() !== "" &&
+        editFieldCredits.value.trim() !== "" &&
+        editFieldProfessor.value.trim() !== "") {
+        popEdit.removeAttribute('disabled');
+    } else {
+        popEdit.setAttribute('disabled', 'disabled');
+    }
+}
 
 
-//event listeners to update add class button
-document.querySelector('#class-name').addEventListener('change', addClassButton);
-document.querySelector('#start-time').addEventListener('change', addClassButton);;
-document.querySelector('#end-time').addEventListener('change', addClassButton);;
-document.querySelector('#class-day').addEventListener('change', addClassButton);;
-document.querySelector('#class-location').addEventListener('change', addClassButton);;
-document.querySelector('#class-professor').addEventListener('change', addClassButton);;
-document.querySelector('#class-credits').addEventListener('change', addClassButton);;
 
-//event listeners
+//event listeners to add class button
+document.querySelector('#class-name').addEventListener('change', disableAddClassBttn);
+document.querySelector('#start-time').addEventListener('change', disableAddClassBttn);
+document.querySelector('#end-time').addEventListener('change', disableAddClassBttn);
+document.querySelector('#class-day').addEventListener('change', disableAddClassBttn);
+document.querySelector('#class-location').addEventListener('change', disableAddClassBttn);
+document.querySelector('#class-professor').addEventListener('change', disableAddClassBttn);
+document.querySelector('#class-credits').addEventListener('change', disableAddClassBttn);
+
+//event listeners to edit class button
+editFieldName.addEventListener('change', disableEditClassBttn);
+editFieldStartTime.addEventListener('change', disableEditClassBttn);
+editFieldEndTime.addEventListener('change', disableEditClassBttn);
+editFieldDay.addEventListener('change', disableEditClassBttn);
+editFieldLocation.addEventListener('change', disableEditClassBttn);
+editFieldProfessor.addEventListener('change', disableEditClassBttn);
+editFieldCredits.addEventListener('change', disableEditClassBttn);
+
+//event listeners for the buttons in the popup
+
+// Add/create
 popAdd.addEventListener('click', createNewClass);
 popup.addEventListener('click', addpopUp);
+popAddClose.addEventListener('click', addpopUp);
+popAddClose.addEventListener('click', addpopUp);
 popClear.addEventListener('click', clearAddFields);
-popEdit.addEventListener('click', editpopUp);
+
+// Edit
+popEdit.addEventListener('click', editClass);
 popEditClear.addEventListener('click', editpopUp);
-popAddClose.addEventListener('click', addpopUp);
-popAddClose.addEventListener('click', addpopUp);
 popEditClose.addEventListener('click', editpopUp);
