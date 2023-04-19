ce6dbb3072350044ffa9bbc1217c584959e51e2f
CursedMushroom <osl2877@rit.edu>
2023-04-19 16:08:23 -0400

    got cells to have correct data when a new one is added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { display } from './visualize/display.js';
 
 //pop up varibles
 const popAdd = document.querySelector('#pop-media-add');
+popAdd.setAttribute('disabled', 'disabled');
 const popEdit = document.querySelector('#pop-media-edit');
 const popAddClose = document.querySelector('#pop-media-cancel');
 const popEditClose = document.querySelector('#pop-media-editcancel');
@@ -21,16 +22,6 @@ document.querySelectorAll(".classInfo").forEach(
     })
 )
 
-
-//toggle popup on click
-const addpopUp = () => {
-    document.querySelector('#popup-1').classList.toggle('active');
-};
-const editpopUp = () => {
-    document.querySelector('#popup-2').classList.toggle('active');
-}
-
-
 let scheduleA =
     Schedule.create([
         {
@@ -45,10 +36,10 @@ let scheduleA =
         },
         {
             name: "classOG",
-            id: 123,
+            id: 128,
             startTime: '19:13',
             endTime: '20:10',
-            day: "Monday",
+            day: "Tuesday",
             credits: 4,
             location: "MSS-1275",
             professor: "Travis",
@@ -68,14 +59,32 @@ let scheduleA =
                 name: "classB",
                 startTime: '05:40',
                 endTime: '22:20',
-                day: "Monday",
+                day: "Friday",
                 credits: 5,
                 location: "MSS-1275",
                 professor: "Travis",
             }
         ]);
 
+const clearAddFields = () => {
+    document.querySelector('#class-name').value = "";
+    document.querySelector('#start-time').value = '';
+    document.querySelector('#end-time').value = '';
+    document.querySelector('#class-day').value = '';
+    document.querySelector('#class-location').value = '';
+    document.querySelector('#class-professor').value = '';
+    document.querySelector('#class-credits').value = '';
 
+}
+
+//toggle popup on click
+const addpopUp = () => {
+    document.querySelector('#popup-1').classList.toggle('active');
+    clearAddFields();
+};
+const editpopUp = () => {
+    document.querySelector('#popup-2').classList.toggle('active');
+}
 
 const createNewClass = () => {
     scheduleA =
@@ -92,69 +101,100 @@ const createNewClass = () => {
                     professor: document.querySelector('#class-professor').value
                 }
             ]);
-
+    // remove all current rows
+    document.querySelectorAll(".rowOfClasses").forEach(
+        (r) => { r.remove() }
+    );
     const res = display()(scheduleA);
     if (Schedule.validate(res.oldSchedule)) {
         console.log(res);
-        let newRow = scheduleTable.insertRow(currentRow++);
-        for (let i = 0; i < daysOfTheWeek.length; i++) {
-            if (daysOfTheWeek[i] === document.querySelector('#class-day').value.toLowerCase()) {
+        // make array of classers per day
+        const monClasses = res.filter((c) => c.day === "Monday");
+        const tuesClasses = res.filter((c) => c.day === "Tuesday");
+        const wedClasses = res.filter((c) => c.day === "Wednesday");
+        const thursClasses = res.filter((c) => c.day === "Thursday");
+        const friClasses = res.filter((c) => c.day === "Friday");
+        const satClasses = res.filter((c) => c.day === "Saturday");
 
-                let newCell = newRow.insertCell(i);
+        // finding the day with the most classes
+        const numClasses = [monClasses.length, tuesClasses.length, wedClasses.length, thursClasses.length, friClasses.length, satClasses.length];
+        const mostClasses = Math.max(...numClasses);
+
+
+        // make the row elements function
+        const makeCell = (classItem, rowElement) => {
+            if (classItem) {
+                let newCell = rowElement.insertCell();
                 newCell.classList.add("classInfo");
-                //console.log(res.oldSchedule)
+                newCell.setAttribute("id", classItem.id);
                 let tempStart;
                 let tempEnd;
-                if (res[res.length - 1].startTime.split(":")[0] > 12) {
-                    tempStart = Number(res[res.length - 1].startTime) - 12;
+                if (classItem.startTime.split(":")[0] > 12) {
+                    tempStart = Number(classItem.startTime.split(":")[0]) - 12;
                     tempStart.toString();
                     tempStart += 'pm';
                 }
                 else {
-                    tempStart = res[res.length - 1].startTime;
+                    tempStart = classItem.startTime;
                     tempStart.toString();
                     tempStart += 'am';
                 }
 
-                if (res[res.length - 1].endTime.split(":")[0] > 12) {
-                    tempEnd = Number(res[res.length - 1].endTime) - 12;
+                if (classItem.endTime.split(":")[0] > 12) {
+                    tempEnd = Number(classItem.endTime.split(":")[0]) - 12;
                     tempEnd.toString();
                     tempEnd += 'pm';
                 }
                 else {
-                    tempEnd = res[res.length - 1].endTime;
+                    tempEnd = classItem.endTime;
                     tempEnd.toString();
                     tempEnd += 'am';
                 }
-                newCell.innerHTML = `${res[res.length - 1].name} ${tempStart}-${tempEnd}`;
+                newCell.innerHTML = `${classItem.name}, <br> ${tempStart}-${tempEnd} , <br> ${classItem.location} , <br> ${classItem.professor}`;
+            } else {
+                let newCell = rowElement.insertCell();
+                newCell.innerHTML = "";
             }
-            else {
-                let newCell = newRow.insertCell(i);
-                newCell.classList.add("classInfo");
-                newCell.innerHTML = " ";
-            }
+        }
+
+        for (let i = 0; i < mostClasses; i++) {
+            const newRow = scheduleTable.insertRow();
+
+            makeCell(monClasses[i], newRow);
+            makeCell(tuesClasses[i], newRow);
+            makeCell(wedClasses[i], newRow);
+            makeCell(thursClasses[i], newRow);
+            makeCell(friClasses[i], newRow);
+            makeCell(satClasses[i], newRow);
+
         }
     }
     addpopUp();
 }
 
-const clearAddFields = () => {
-    document.querySelector('#class-name').value = "";
-    document.querySelector('#start-time').value = '';
-    document.querySelector('#end-time').value = '';
-    document.querySelector('#class-day').value = '';
-    document.querySelector('#class-location').value = '';
-    document.querySelector('#class-professor').value = '';
-    document.querySelector('#class-credits').value = '';
-
+//disable the addClass button if the fields are empty
+const addClassButton = () => {
+    if (document.querySelector('#class-name').value.trim() !== "" &&
+        document.querySelector('#start-time').value.trim() !== "" &&
+        document.querySelector('#end-time').value.trim() !== "" &&
+        document.querySelector('#class-day').value.trim() !== "" &&
+        document.querySelector('#class-location').value.trim() !== "" &&
+        document.querySelector('#class-professor').value.trim() !== "") {
+        popAdd.removeAttribute('disabled');
+    } else {
+        popAdd.setAttribute('disabled', 'disabled');
+    }
 }
 
-// const res = display()(scheduleA);
-// console.log(Schedule.validate(res.oldSchedule));
 
-
-
-
+//event listeners to update add class button
+document.querySelector('#class-name').addEventListener('change', addClassButton);
+document.querySelector('#start-time').addEventListener('change', addClassButton);;
+document.querySelector('#end-time').addEventListener('change', addClassButton);;
+document.querySelector('#class-day').addEventListener('change', addClassButton);;
+document.querySelector('#class-location').addEventListener('change', addClassButton);;
+document.querySelector('#class-professor').addEventListener('change', addClassButton);;
+document.querySelector('#class-credits').addEventListener('change', addClassButton);;
 
 //event listeners
 popAdd.addEventListener('click', createNewClass);
@@ -162,5 +202,6 @@ popup.addEventListener('click', addpopUp);
 popClear.addEventListener('click', clearAddFields);
 popEdit.addEventListener('click', editpopUp);
 popEditClear.addEventListener('click', editpopUp);
+popAddClose.addEventListener('click', addpopUp);
 popAddClose.addEventListener('click', addpopUp);
 popEditClose.addEventListener('click', editpopUp);
